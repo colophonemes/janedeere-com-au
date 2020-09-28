4eb892919fc4879d5ea2775ff4c4806a8d426d95
colophonemes <dev@colophonemes.com>
2020-09-28 18:25:31 +0100

    Add next-sitemap script to postbuild

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://janedeere.com.au',
+  generateRobotsTxt: true,
+}
--- siteGlobals.js
+++ siteGlobals.js
@@ -1,1 +1,2 @@
 export const SITE_TITLE = 'Jane Deere Coaching'
+export const SITE_URL = 'https://janedeerecoaching.com.au'
