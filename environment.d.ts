15888fb27f0da683a9c706f9977f28fb6006a009
colophonemes <dev@colophonemes.com>
2020-09-28 16:14:53 +0100

    Dummy commit to force Vercel to deploy

--- environment.d.ts
+++ environment.d.ts
@@ -4,8 +4,8 @@ declare global {
       NODE_ENV: 'development' | 'production'
       CONTENTFUL_SPACE: string
       CONTENTFUL_CONTENT_DELIVERY_ACCESS_TOKEN: string
-      NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID: string
       NEXT_PUBLIC_STATICKIT_SITE_ID: string
+      NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID: string
     }
   }
 }
