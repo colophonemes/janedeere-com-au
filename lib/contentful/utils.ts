ab895e783b01c050b4010e2cc43398fb57637a79
colophonemes <dev@colophonemes.com>
2020-09-28 16:37:03 +0100

    Update typings to fix compiler error

--- lib/contentful/utils.ts
+++ lib/contentful/utils.ts
@@ -1,12 +1,16 @@
 import { EntryCollection, Entry } from 'contentful'
-import { TContentfulEntry } from './'
+import { TContentfulEntry, TContentfulEntryFields } from './'
 
-export const flattenEntryResponse = (
-  entries: EntryCollection<TContentfulEntry>
-) => entries.items
+export function flattenEntryResponse<T extends TContentfulEntryFields>(
+  entries: EntryCollection<T>
+) {
+  return entries.items
+}
 
-export const flattenEntryResponseSingle = (
-  entries: EntryCollection<TContentfulEntry>
-) => flattenEntryResponse(entries)[0]
+export function flattenEntryResponseSingle<T extends TContentfulEntryFields>(
+  entries: EntryCollection<T>
+) {
+  return flattenEntryResponse(entries)[0]
+}
 
-export const flattenEntry = (entry: Entry<TContentfulEntry>) => entry.fields
+export const flattenEntry = (entry: TContentfulEntry) => entry.fields
