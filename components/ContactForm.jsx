69a1cded4c5b88c0a49554137cff990e1a9e0a2e
colophonemes <dev@colophonemes.com>
2021-01-28 10:50:18 +0000

    Replace Statickit with Formspree

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -3,7 +3,7 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { makeStyles } from '@material-ui/styles'
-import { useForm } from '@statickit/react'
+import { useForm } from '@formspree/react'
 import TextField from '@material-ui/core/TextField'
 
 const useStylesThankYou = makeStyles((theme) => ({
