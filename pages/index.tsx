b5edcb420280e79989e13fd86c8fb39b42ef65fc
colophonemes <dev@colophonemes.com>
2021-05-17 13:05:14 +0100

    Revalidate home page every minute

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import ContentfulPage from './[slug]'
 import { fetchPage } from 'lib/contentful/queries'
+import { GetStaticProps } from 'next'
 
 export default ContentfulPage
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const data = await fetchPage('home')
   return {
     props: {
       data,
     },
+    revalidate: 60,
   }
 }
