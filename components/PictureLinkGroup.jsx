42a7eae6e83b51c57957758e77a6def01d05a517
colophonemes <dev@colophonemes.com>
2023-11-09 21:01:13 +0000

    Picture links are always 2 items across

--- components/PictureLinkGroup.jsx
+++ components/PictureLinkGroup.jsx
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types'
 import PictureLink from 'components/PictureLink'
 import Grid from '@material-ui/core/Grid'
 
-const PictureLinkGroup = ({ sys, fields }) => <Grid container justify='center' spacing={3}>
-  {fields.pictureLinks.map(pictureLink => <Grid item key={pictureLink.sys.id} xs={6} md={4}>
-    <PictureLink {...pictureLink} />
-  </Grid>)}
-</Grid>
+const PictureLinkGroup = ({ sys, fields }) => (
+  <Grid container justify="center" spacing={3}>
+    {fields.pictureLinks.map((pictureLink) => (
+      <Grid item key={pictureLink.sys.id} xs={6}>
+        <PictureLink {...pictureLink} />
+      </Grid>
+    ))}
+  </Grid>
+)
 
 PictureLinkGroup.propTypes = {
   sys: PropTypes.object.isRequired,
-  fields: PropTypes.object.isRequired
+  fields: PropTypes.object.isRequired,
 }
 
 export default PictureLinkGroup
